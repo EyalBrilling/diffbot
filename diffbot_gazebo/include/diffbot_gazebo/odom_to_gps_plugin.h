a5cdba9b0b3a09a01696d230931da5fbe7bc0f3c
Eyal Brilling <eyalbrilling@gmail.com>
2023-12-05 13:39:35 +0200

    - finished plugin

--- diffbot_gazebo/include/diffbot_gazebo/odom_to_gps_plugin.h
+++ diffbot_gazebo/include/diffbot_gazebo/odom_to_gps_plugin.h
@@ -6,43 +6,48 @@
 #include <gazebo/transport/TransportTypes.hh>
 #include <gazebo/common/Time.hh>
 #include <gazebo/common/Plugin.hh>
+#include <gazebo/common/common.hh>
 #include <gazebo/common/Events.hh>
 
 #include <sensor_msgs/NavSatFix.h>
 #include <nav_msgs/Odometry.h>
 
+// Callback queue for subscriber
+#include <ros/callback_queue.h>
 
 namespace gazebo
 {
 
-   class OdomToGpsPlugin : public ModelPlugin
+   class OdomToGpsPlugin : public WorldPlugin
    {
       public:
       OdomToGpsPlugin();
       virtual ~OdomToGpsPlugin();
 
      protected:
-     void Load( physics::ModelPtr _parent, sdf::ElementPtr _sdf );
+     void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
      void onUpdate();
      
      private:
         ros::NodeHandle* node_handle_;
         ros::Subscriber odom_subscriber_;
         ros::Publisher gps_publisher_;
-
+        void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
+        
     // Pointer to the model
-    private: physics::ModelPtr model;
- 
-    // Pointer to the update event connection
-    private: event::ConnectionPtr updateConnection;
+        physics::ModelPtr model;
      
-    // Time Memory
-    double old_secs;
+     // gazebo connection
+     event::ConnectionPtr updateConnection;
+
+    std::string namespace_;
 
     // Save messages 
      sensor_msgs::NavSatFix gps_info;
      nav_msgs::Odometry odom_info;
 
+    // Queue
+    ros::CallbackQueue queue_;
 
    };
 
--- diffbot_gazebo/src/odom_to_gps_plugin.cpp
+++ diffbot_gazebo/src/odom_to_gps_plugin.cpp
@@ -3,40 +3,56 @@
 
 namespace gazebo {
 
-void OdomToGpsPlugin::Load( physics::ModelPtr _parent, sdf::ElementPtr _sdf ){
-    // Store the pointer to the model
-      this->model = _parent;
- 
-      // Listen to the update event. This event is broadcast every
-      // simulation iteration.
-      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-          std::bind(&amp;OdomToGpsPlugin::onUpdate, this));
-       
-      this->old_secs =ros::Time::now().toSec();
+void OdomToGpsPlugin::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf){
+
+    ROS_WARN("Loading odomToGps plugin to gazebo world");
+    ROS_INFO("Received Odometry message");
+
+    // load parameters
+    if (!_sdf->HasElement("robotNamespace"))
+        namespace_.clear();
+    else
+        namespace_ = _sdf->GetElement("robotNamespace")->GetValue()->GetAsString();
       
+
+       this->updateConnection = 
+      event::Events::ConnectWorldUpdateBegin(
+          boost::bind(&OdomToGpsPlugin::onUpdate, this));
+
+      // Topic names
       std::string gps_topic = "/robot_location";
-      std::string odom_topic ="/diffbot/mobile_base_controller/cmd_vel"
+      std::string odom_topic ="/diffbot/mobile_base_controller/odom";
 
-     this->node_handle_.reset(new ros::NodeHandle("odomtogps_rosnode"));
+     node_handle_ = new ros::NodeHandle("odomToGps");
 
-     odom_subscriber_ = this->node_handle_.subscribe(odom_topic, 1000, odomSubCallback);
+
+    ros::SubscribeOptions so =
+      ros::SubscribeOptions::create<nav_msgs::Odometry>(odom_topic, 1,
+          boost::bind(&OdomToGpsPlugin::odomCallback, this, _1),
+          ros::VoidPtr(),&queue_);
+
+    odom_subscriber_ = node_handle_->subscribe(so);
+    gps_publisher_ = node_handle_->advertise<sensor_msgs::NavSatFix>(gps_topic, 1);
+
 }
 
 void OdomToGpsPlugin::onUpdate(){
 
 }
 
-void odomSubCallback(const nav_msgs::Odometry::ConstPtr& msg)
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
  {
-   ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+   ROS_INFO("Received Odometry message. Pose: [%f, %f, %f]", msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
+
 }
 
-GazeboRosGps::GazeboRosGps(){
+OdomToGpsPlugin::OdomToGpsPlugin() : WorldPlugin(){
 }
 
-GazeboRosGps::~GazeboRosGps(){
+OdomToGpsPlugin::~OdomToGpsPlugin(){
 }
 
-GZ_REGISTER_MODEL_PLUGIN(OdomToGpsPlugin)
+GZ_REGISTER_WORLD_PLUGIN(OdomToGpsPlugin)
 
 }
