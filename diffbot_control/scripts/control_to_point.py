d42a6cb3b25c9e8fa89d77fdc2646b25e91128bd
Eyal Brilling <eyalbrilling@gmail.com>
2023-12-06 01:25:55 +0200

    - Problem because of odometry error buildup

--- diffbot_control/scripts/control_to_point.py
+++ diffbot_control/scripts/control_to_point.py
@@ -11,6 +11,8 @@ from math import atan2
 LATITUDE_TO_METER = 111000
 LONGITUDE_TO_METER = 73000
 
+TARGET_STOP_RANGE_METER = 1
+
 # starting robot position
 xStartRobotLoc = 0.0
 yStartRobotLoc = 0.0
@@ -30,21 +32,22 @@ goal = Point(0,0,0)
 
 def odomCallback(msg):
     global robotLoc
-    global orientation
-
+    global theta
     robotLoc.x = msg.pose.pose.position.x
     robotLoc.y = msg.pose.pose.position.y
     orientation = msg.pose.pose.orientation
-    (_1,_2, theta) = euler_from_quaternion([orientation.x,orientation.y,orientation.z,orientation.w]) 
+    (roll,pitch, theta) = euler_from_quaternion([orientation.x,orientation.y,orientation.z,orientation.w]) 
 
+    
 def targetCallback(msg):
-    global xGoal,yGoal
-    xGoal,yGoal = calculateXYtarget(msg.latitude,msg.longitude)
+    global goal
+    goal.x,goal.y = calculateXYtarget(msg.latitude,msg.longitude)
+
 
 # Calculate x and y in sim based on diff between starting and target geoPoints
 def calculateXYtarget(targetLatitude,targetLongitude):
     latDiff = targetLatitude - latStartRobotLoc
-    longDiff = targetLongitude - latStartRobotLoc
+    longDiff = targetLongitude - longStartRobotLoc
     targetX = latDiff * LATITUDE_TO_METER
     targetY = longDiff * LONGITUDE_TO_METER
     return targetX,targetY
@@ -54,19 +57,20 @@ rospy.init_node("to_point_controller")
 odomSub = rospy.Subscriber("/diffbot/mobile_base_controller/odom",Odometry,odomCallback)
 targetSub = rospy.Subscriber("/target_location",GeoPoint,targetCallback)
 robotTwistPub = rospy.Publisher("/diffbot/mobile_base_controller/cmd_vel",Twist,queue_size=1)
-rate = rospy.Rate(10)
+rate = rospy.Rate(100)
 #robotGpsLocationSub= rospy.Subscriber("/robot_location")
-
+ 
 robotTwist = Twist()
 
 while not rospy.is_shutdown():
     xDiffRobotTarget = goal.x - robotLoc.x
     yDiffRobotTarget = goal.y - robotLoc.y
-
     angle_to_goal = atan2(yDiffRobotTarget , xDiffRobotTarget)
-    if abs(angle_to_goal - theta) > 0.05:
+    if abs(angle_to_goal - theta) > 0.1:
+        robotTwist.linear.x = 0.0
         robotTwist.angular.z = 0.3
     else:
+        robotTwist.linear.x = 0.5
         robotTwist.angular.z = 0.0
     robotTwistPub.publish(robotTwist)
     rate.sleep()
