0b5e9c832452bd8732a6fe9c3968fcad56f6fe28
Eyal Brilling <eyalbrilling@gmail.com>
2023-12-05 21:02:02 +0200

    - Add needed packages > math and tf for orientation and distance calc > The rest for ros nodes and msgs

--- diffbot_control/scripts/control_to_point.py
+++ diffbot_control/scripts/control_to_point.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
 import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Twist
+from geographic_msgs.msg import GeoPoint
+from tf.transformations import euler_from_quaternion
+from math import atan2
