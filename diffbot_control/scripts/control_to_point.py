786e3715caf960d43b7791cb6d30bbeffc11e1ee
Eyal Brilling <eyalbrilling@gmail.com>
2023-12-05 21:04:48 +0200

    - ros pubs and subs needed

--- diffbot_control/scripts/control_to_point.py
+++ diffbot_control/scripts/control_to_point.py
@@ -5,3 +5,10 @@ from geometry_msgs.msg import Point, Twist
 from geographic_msgs.msg import GeoPoint
 from tf.transformations import euler_from_quaternion
 from math import atan2
+
+rospy.init_node("to_point_controller")
+odomSub = rospy.Subscriber("/diffbot/mobile_base_controller/odom",Odometry,odomCallback)
+targetSub = rospy.Subscriber("/target_location",GeoPoint,targetCallback)
+robotTwistPub = rospy.Publisher("/diffbot/mobile_base_controller/cmd_vel",Twist,queue_size=1)
+rate = rospy.Rate(10)
+#robotGpsLocationSub= rospy.Subscriber("/robot_location")
